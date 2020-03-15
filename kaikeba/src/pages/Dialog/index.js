7e453b21b7a6ca01ac4ecbaf7503de1771d49c98
Gu-Miao <1450206741@qq.com>
2020-03-16 00:32:28 +0800

    add pages

---
+++ kaikeba/src/pages/Dialog/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Layout from '@/components/Layout'
+import { ModalClass } from '@/components/Modal'
+
+export default function Dialog() {
+  const [showClassModdal, setShowClassModdal] = useState(false)
+  const [showFuncModal, setShowFuncModal] = useState(false)
+  return (
+    <Layout>
+      <h1>Dialog</h1>
+      <button
+        onClick={() => {
+          setShowClassModdal(true)
+        }}
+      >
+        open class modal
+      </button>
+      <button
+        onClick={() => {
+          setShowFuncModal(true)
+        }}
+      >
+        open func modal
+      </button>
+      <ModalClass
+        show={showClassModdal}
+        onClose={() => {
+          setShowClassModdal(false)
+        }}
+        title="my class dialog"
+        body={<div>红红火火恍恍惚惚</div>}
+        footer={<button>ok</button>}
+      />
+      <ModalClass
+        show={showFuncModal}
+        onClose={() => {
+          setShowFuncModal(false)
+        }}
+        title="my func dialog"
+        body={<div>函数组件存在问题</div>}
+        footer={<button>ok</button>}
+      />
+    </Layout>
+  )
+}
