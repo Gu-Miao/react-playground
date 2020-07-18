f7ff5a556603871ebe292676231c0ce30425b6d9
Gu-Miao <1450206741@qq.com>
2020-07-18 16:54:06 +0800

    remove useless projects

--- easy-book/src/actions/action-types.js
+++
@@ -1,7 +0,0 @@
-export const SEARCH_FOCUS = 'search_focus';
-export const SEARCH_BLUR = 'search_blur';
-export const REFRESH_HOT_LIST = 'refresh_hot_list';
-export const PANEL_MOUSEENTER = 'panel_mouseenter';
-export const PANEL_MOUSEOUT = 'panel_mouseout';
-export const GET_DETAIL = 'get_detail';
-export const LOGIN = 'login';
