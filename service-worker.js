865cbdfb6cdad2f6ed84c39a53ea3838fc6e66d0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-24 16:10:14 +0000

    Deploy Gu-Miao/react-playground to github.com/Gu-Miao/react-playground.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e3fb62857ce4dfeed5e2440dfeb0b60.js"
+  "./precache-manifest.96e75b59f1e824119e3f261601735464.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
