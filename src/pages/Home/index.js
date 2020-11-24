b956b855036354e9724abbf861abcae84549a667
Gu-Miao <1450206741@qq.com>
2020-11-24 20:03:09 +0800

    modify description

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,8 @@ import Layout from '@/components/Layout'
 export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
-      <h2>Kaike8 lessons practices</h2>
+      <h1>React playground</h1>
+      <h2>react demo collections</h2>
     </Layout>
   )
 }
